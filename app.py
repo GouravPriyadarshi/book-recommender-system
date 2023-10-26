cfe377419f36f7edac7f4a51145d6431c9468cda
GouravPriyadarshi <101411086+GouravPriyadarshi@users.noreply.github.com>
2023-10-26 10:33:42 +0530

    Files upload

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request
+import pickle
+import pandas
+import numpy as np
+
+popular_df = pickle.load(open('popular.pkl', 'rb'))
+pt = pickle.load(open('pt.pkl', 'rb'))
+books = pickle.load(open('books.pkl', 'rb'))
+similarity_score = pickle.load(open('similarity_scores.pkl', 'rb'))
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           book_name=popular_df['Book-Title'].to_list(),
+                           author=popular_df['Book-Author'].to_list(),
+                           image=popular_df['Image-URL-M'].to_list(),
+                           votes=popular_df['num_ratings'].to_list(),
+                           rating=popular_df['avg_rating'].to_list()
+                           )
+
+
+@app.route('/recommend')
+def recommend_ui():
+    return render_template('recommend.html')
+
+
+@app.route('/contact')
+def contact_ui():
+    return render_template('contact.html')
+
+
+@app.route('/recommend_books', methods=['POST'])
+def recommend():
+    user_input = request.form.get('user_input')
+    index = np.where(pt.index == user_input)[0][0]
+    # nearest book as per Euclidean distance also known as similar to the book
+    similar_items = sorted(list(enumerate(similarity_score[index])), key=lambda x: x[1], reverse=True)[1:5]
+
+    # Change
+
+    # getting only the index of the similar books
+    data = []
+    for i in similar_items:
+        item = []
+        temp_df = books[books['Book-Title'] == pt.index[i[0]]].copy()  # Create a copy of the subset
+        temp_df = temp_df.drop_duplicates(subset='Book-Title')  # Drop duplicates and assign back to temp_df
+        item.extend(temp_df['Book-Title'].values)
+        item.extend(temp_df['Book-Author'].values)
+        item.extend(temp_df['Image-URL-M'].values)
+        data.append(item)
+
+    print(data)
+
+    return render_template('recommend.html', data=data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
